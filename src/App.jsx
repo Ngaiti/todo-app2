f13196e6dffb128d5ea559a96378a850b44ee0d3
Ngaiti <henfanar@gmail.com>
2023-06-09 09:57:00 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container, Navbar } from "react-bootstrap"
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom"
+import ErrorPage from "./pages/ErrorPage"
+import Home from "./pages/Home"
+import './App.css'
+
+
+function Layout() {
+  return (
+    <>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="/"> Todos </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </>
+  )
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
